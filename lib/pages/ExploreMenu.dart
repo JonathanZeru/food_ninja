985bccf6ac18e5a17dc23af0d2a7ce5132497d56
Abel Shibabaw <abelshibabaw@heuc.edu.et>
2023-03-04 20:58:43 +0300

    just started my second page try to put the navigation bar i don't have any idea how too

--- lib/pages/ExploreMenu.dart
+++ lib/pages/ExploreMenu.dart
@@ -5,6 +5,7 @@ import 'package:food_ninja/res/cover_text.dart';
 import 'package:food_ninja/res/Icon_notification.dart';
 import 'package:food_ninja/res/search.dart';
 import 'package:food_ninja/res/filter_icon.dart';
+import 'package:food_ninja/res/restaurant.dart';
 
 class ExploreMenu extends StatefulWidget {
   const ExploreMenu({super.key});
@@ -68,74 +69,7 @@ class _ExploreMenuState extends State<ExploreMenu> {
                       ),
                     ],
                   ),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18),
-                    ),
-                    elevation: 20,
-                    shadowColor: Colors.transparent,
-                    color: Colors.white,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 20),
-                      child: Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SizedBox(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    width: 200,
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Container(
-                                            child: Image.asset(
-                                                'assets/Menu Photo.png')),
-                                        Column(
-                                          children: const [
-                                            Text(
-                                              'Herbal Pancake',
-                                              style: TextStyle(
-                                                fontFamily: 'BentonSans Medium',
-                                                fontSize: 24,
-                                              ),
-                                            ),
-                                            Text(
-                                              'Warung Herbal',
-                                              style: TextStyle(
-                                                  fontFamily:
-                                                      'BentonSans Regular',
-                                                  fontSize: 19,
-                                                  color: Color.fromARGB(
-                                                      255, 101, 101, 101)),
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            SizedBox(
-                              child: Text(
-                                '\$7',
-                                style: TextStyle(
-                                  fontFamily: 'BentonSans Bold',
-                                  fontSize: 26,
-                                  color: Color.fromARGB(255, 254, 173, 29),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+                  Restaurant(),
                 ],
               ),
             ),
@@ -145,3 +79,4 @@ class _ExploreMenuState extends State<ExploreMenu> {
     );
   }
 }
+
