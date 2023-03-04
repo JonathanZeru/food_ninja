034dc54b5ae8b485886f280c2be72fc26022c112
Dagim Tesfaye Mebrate <102017186+DagNo1@users.noreply.github.com>
2023-03-04 12:55:06 +0300

    Initial Commit

---
+++ food_ninja/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() { runApp( const MaterialApp( //TODO REMOVE CONST <--------
+    initialRoute: '/',
+    routes: {
+      // '/': (context) => const CLass_Name(), //change Class name with your page widget and import the widget
+    },
+  ));
+}
+/*TODO
+    READ ME!!!!!!!!!!!!!!!!!!!!!!!!
+     - Put all images in the assets folder.
+      if they are general like a logo for example put them in the assets folder directly
+      but if they are specific to a page for example the image of the restaurant in the page 'detail product' put it in a new folder called 'detail_product.
+     - Add all custom widgets in the res folder by creating a new dart file for example a custom button would have button.dart file in the res folder
+     - Put notes here if you want
+*/
